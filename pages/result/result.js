397612b2afce672546648170efba89dfa23f7d6c
Ashkan Yadegari <ashkan.yadegari@outlook.com>
2020-12-04 08:49:14 +0800

    nightfix

---
+++ pages/result/result.js
@@ -0,0 +1,80 @@
+// pages/result/result.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+    const params = {user_id: 1, course_id: options.course}
+    let page = this 
+    wx.request({
+      url: 'http://localhost:3000/api/v1/useranswer',
+      data: params,
+      method: 'GET',
+      success(res){
+        console.log(res)
+        const results = res.data
+        page.setData(results)
+
+      }
+    })
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
