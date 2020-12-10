fd9967378b8be98d05b6daffb5e2a49eed0c0c2b
Ashkan Yadegari <ashkan.yadegari@outlook.com>
2020-12-10 11:03:17 +0800

    v

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ App({
   },
   
   globalData: {
-     host: 'http://localhost:3000/',
-  //  host : 'https://aixuexi.wogengapp.cn/',
+    //  host: 'http://localhost:3000/',
+   host : 'https://aixuexi.wogengapp.cn/',
    // ## place holder for dooku server ##
   }
 })
